fa6dd1ef935068774c11bd3692adaac922bfe125
SunsEmbrace <moyasinkhan2000@gmail.com>
2025-11-22 00:30:50 +0000

    Dynamic blocks practice with ingress for sg

--- DeleteThisLaterTestconfig.tf
+++ DeleteThisLaterTestconfig.tf
@@ -12,3 +12,31 @@ provider "aws" {
 resource "aws_compute_instance" "aliasinstance" {
     provider = aws.europe 
 }
+
+locals {
+ingress_rules = [{
+    port = 443
+    description = "Port 443"
+},
+{
+    port = 80
+    description = "port 80"
+}]
+
+}
+resource "aws_security_group" "main" {
+    name = "sg"
+    vpc_id= data.aws_vpc.main.id
+
+    dynamic "ingress" {
+        for_each = local.ingress_rules
+
+        content {
+            description = ingress.value.description
+            from_port = ingress.value.port
+            to_port = ingrress.value.port
+            protocol = "tcp"
+            cidr_blocks = {"0.0.0.0/0"}
+        }
+    }
+}
